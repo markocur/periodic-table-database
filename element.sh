4920dc2ae4a75df42cecf07be835bd2e4b582e43
markocur <mariusz.m.koch@gmail.com>
2023-04-06 13:17:30 +0000

    fix: nicer formatting of the result

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t -c"
 
 if [[ -z $1 ]]
 then
@@ -16,17 +16,17 @@ else
     #provide info to the user
     INFO=$($PSQL "
     select 
-      e.symbol,e.name,p.atomic_mass,p.melting_point,p.boiling_point,t.type 
+      e.atomic_number,e.symbol,e.name,p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius,t.type 
     from 
       elements e
-      join properties p on p.atomic_mass = e.atomic_mass
+      join properties p on p.atomic_number = e.atomic_number
       join types t on t.type_id = p.type_id
     where
       e.atomic_number = $ATOMIC_NUMBER
     ")
-    echo $INFO | while read SYM BAR NAME BAR MASS BAR MPOINT BAR BPOINT BAR TYPE
+    echo "$INFO" | while read NUM BAR SYM BAR NAME BAR MASS BAR MPOINT BAR BPOINT BAR TYPE
     do
-      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYM). It's a $TYPE, with a mass of $MASS. $NAME has a melting point of $MPOINT celsius and a boiling point of $BPOINT celsius."
+      echo "The element with atomic number $NUM is $NAME ($SYM). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MPOINT celsius and a boiling point of $BPOINT celsius."
     done
   fi
 fi
